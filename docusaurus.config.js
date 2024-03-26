76d246a9b394701d12bfa8f1b2770e67fd29bd7b
Web3-Jerry <151769490+Web3-Jerry@users.noreply.github.com>
2024-03-26 09:55:39 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,7 +41,7 @@ const config = {
       ({
         docs: {
           // id: 'product', // omitted => default instance
-          id: 'default',
+          id: 'docs',
           path: 'docs',
           routeBasePath: 'docs',
           sidebarPath: './sidebars.js',
